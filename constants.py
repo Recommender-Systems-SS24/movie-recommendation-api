ada9f97f04356d163f700ff7db2b23782a06713f
clarari <cmricken2@gmail.com>
2024-06-08 12:29:21 +0200

    try out various rec algos

--- constants.py
+++ constants.py
@@ -5,7 +5,7 @@ RATINGS_CSV_FILE_PATH = '..\\ratings.csv'
 # path to directory containing the json movie metadata files
 MOVIES_METADATA_JSON_DIR_PATH = '.\\movies_details'
 
-MOVIES_METADATA_PARQUET_FILE_PATH = 'movies_metadata.parquet'
-ITEM_USER_MATRIX_PARQUET_FILE_PATH = 'item_user_matrix.parquet'
-DESCRIPTION_COSINE_SIM_MATRIX_NPY_FILE_PATH = 'description_cosine_sim_matrix.npy'
-TITLE_COSINE_SIM_MATRIX_NPY_FILE_PATH = 'title_cosine_sim_matrix.npy'
+MOVIES_METADATA_PARQUET_FILE_PATH = '.\\data\\movies_metadata.parquet'
+ITEM_USER_MATRIX_PARQUET_FILE_PATH = '.\\data\\item_user_matrix.parquet'
+DESCRIPTION_COSINE_SIM_MATRIX_NPY_FILE_PATH = '.\\data\\description_cosine_sim_matrix.npy'
+TITLE_COSINE_SIM_MATRIX_NPY_FILE_PATH = '.\\data\\title_cosine_sim_matrix.npy'
--- recommender.py
+++ recommender.py
@@ -1,13 +1,30 @@
 import loader
 import content_based.recommendations as cbr
-from content_based.similarity_measures.text_based import title_cosine_similarity
+import collaborative_filtering.recommendations as cfr
+from content_based.similarity_measures.custom import genre_custom_similarity
+from content_based.similarity_measures.set_based import set_based_similarity, jaccard_index, SetExtractor
+from content_based.similarity_measures.text_based import title_cosine_similarity, description_cosine_similarity
 
 
 def get_recommendations(movie_id):
     # TODO: use different recommendation algorithms
-    rec_1 = cbr.get_similar_movies(movie_id, [{'weight': 1, 'measure': title_cosine_similarity}])
+    rec_1 = cbr.get_similar_movies(movie_id, [{'weight': 1,
+                                               'measure': set_based_similarity(jaccard_index, SetExtractor.KEYWORD)}])
     rec_1_enriched = [enrich_with_data(rec[0]) for rec in rec_1]
-    return [rec_1_enriched, rec_1_enriched, rec_1_enriched, rec_1_enriched, rec_1_enriched]
+
+    rec_2 = cfr.get_movie_recommendations(movie_id)
+    rec_2_enriched = [enrich_with_data(rec[0]) for rec in rec_2]
+
+    rec_3 = cbr.get_similar_movies(movie_id, [{'weight': 1, 'measure': genre_custom_similarity}])
+    rec_3_enriched = [enrich_with_data(rec[0]) for rec in rec_3]
+
+    rec_4 = cbr.get_similar_movies(movie_id, [{'weight': 1, 'measure': title_cosine_similarity}])
+    rec_4_enriched = [enrich_with_data(rec[0]) for rec in rec_4]
+
+    rec_5 = cbr.get_similar_movies(movie_id, [{'weight': 1, 'measure': description_cosine_similarity}])
+    rec_5_enriched = [enrich_with_data(rec[0]) for rec in rec_5]
+
+    return [rec_1_enriched, rec_2_enriched, rec_3_enriched, rec_4_enriched, rec_5_enriched]
 
 
 def enrich_with_data(movie_id):
@@ -30,4 +47,3 @@ def get_random_recommendations():
         recs = [enrich_with_data(movie_id) for movie_id in random_movie_ids]
         recommendations.append(recs)
     return recommendations
-
