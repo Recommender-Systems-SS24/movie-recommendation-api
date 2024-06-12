fea3d833102b4ef480130c69df34e7c3925a50a8
clarari <cmricken2@gmail.com>
2024-06-12 12:04:59 +0200

    split up request

--- recommender.py
+++ recommender.py
@@ -7,31 +7,24 @@ from content_based.similarity_measures.set_based import set_based_similarity, ja
 from content_based.similarity_measures.text_based import title_cosine_similarity, description_cosine_similarity
 
 
-def get_recommendations(movie_id):
+def get_recommendations(movie_id, list_number):
     # TODO: choose final recommendations algorithms to be used in the presentation
-    rec_1 = cbr.get_similar_movies(movie_id, [{'weight': 1,
-                                               'measure': set_based_similarity(jaccard_index, SetExtractor.KEYWORD)}])
-    rec_1_enriched = [enrich_with_data(rec[0]) for rec in rec_1]
 
-    rec_2 = cfr.get_movie_recommendations(movie_id)
-    if len(rec_2) < 1:
-        # it is not possible to perform item-based collaborative filtering for all movies, as cf is done with a
-        # smaller dataset
-        print("director set intersection cardinality was used for movie with id {}".format(movie_id))
-        rec_2 = cbr.get_similar_movies(movie_id, [
-            {'weight': 1, 'measure': set_based_similarity(intersection_cardinality, SetExtractor.DIRECTOR)}])
-    rec_2_enriched = [enrich_with_data(rec[0]) for rec in rec_2]
+    if list_number:
+        if list_number == 1:
+            return get_recommendation_list_1(movie_id)
+        elif list_number == 2:
+            return get_recommendation_list_2(movie_id)
+        elif list_number == 3:
+            return get_recommendation_list_3(movie_id)
+        elif list_number == 4:
+            return get_recommendation_list_4(movie_id)
+        elif list_number == 5:
+            return get_recommendation_list_5(movie_id)
 
-    rec_3 = cbr.get_similar_movies(movie_id, [{'weight': 1, 'measure': genre_custom_similarity}])
-    rec_3_enriched = [enrich_with_data(rec[0]) for rec in rec_3]
-
-    rec_4 = cbr.get_similar_movies(movie_id, [{'weight': 1, 'measure': title_cosine_similarity}])
-    rec_4_enriched = [enrich_with_data(rec[0]) for rec in rec_4]
-
-    rec_5 = cbr.get_similar_movies(movie_id, [{'weight': 1, 'measure': description_cosine_similarity}])
-    rec_5_enriched = [enrich_with_data(rec[0]) for rec in rec_5]
-
-    return [rec_1_enriched, rec_2_enriched, rec_3_enriched, rec_4_enriched, rec_5_enriched]
+    return [get_recommendation_list_1(movie_id)['List'], get_recommendation_list_2(movie_id)['List'],
+            get_recommendation_list_3(movie_id)['List'], get_recommendation_list_4(movie_id)['List'],
+            get_recommendation_list_5(movie_id)['List']]
 
 
 def get_recommendation_list_1(movie_id):
