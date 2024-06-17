3c1aad9d7f47474823561785a19804873aca7d9e
clarari <cmricken2@gmail.com>
2024-06-17 18:03:53 +0200

    clean-up and reordering of recommendation lists

--- recommender.py
+++ recommender.py
@@ -9,8 +9,6 @@ import gpt_recommandation.recommandations as gpt
 
 
 def get_recommendations(movie_id, list_number):
-    # TODO: choose final recommendations algorithms to be used in the presentation
-
     if list_number:
         if list_number == 1:
             return get_recommendation_list_1(movie_id)
@@ -23,63 +21,62 @@ def get_recommendations(movie_id, list_number):
         elif list_number == 5:
             return get_recommendation_list_5(movie_id)
 
-    return [get_recommendation_list_1(movie_id)['List'], get_recommendation_list_2(movie_id)['List'],
-            get_recommendation_list_3(movie_id)['List'], get_recommendation_list_4(movie_id)['List'],
-            get_recommendation_list_5(movie_id)['List']]
-
 
 def get_recommendation_list_1(movie_id):
-    rec = cbr.get_similar_movies(movie_id, [{'weight': 1,
-                                             'measure': set_based_similarity(jaccard_index, SetExtractor.KEYWORD)}])
+    rec = gpt.get_similar_movies(movie_id)
+    if len(rec) < 5:
+        # it is not always possible to retrieve 5 movie recommendations from the API
+        print("director set and writer set intersection cardinality was used for movie with id {}".format(movie_id))
+        rec = cbr.get_similar_movies(movie_id, [
+            {'weight': 0.5, 'measure': set_based_similarity(intersection_cardinality, SetExtractor.DIRECTOR)},
+            {'weight': 0.5, 'measure': set_based_similarity(intersection_cardinality, SetExtractor.WRITER)}])
+        rec_enriched = [enrich_with_data(rec[0]) for rec in rec]
+        return {'Name': 'Intersection Cardinality Directors and Writers', 'List': rec_enriched}
     rec_enriched = [enrich_with_data(rec[0]) for rec in rec]
-    return {'Name': 'Keywords', 'List': rec_enriched}
+    return {'Name': 'GPT Recommendations', 'List': rec_enriched}
 
 
 def get_recommendation_list_2(movie_id):
     rec = cbr.get_similar_movies(movie_id, [{'weight': 1, 'measure': genre_custom_similarity}])
     rec_enriched = [enrich_with_data(rec[0]) for rec in rec]
-    return {'Name': 'Genres custom', 'List': rec_enriched}
+    return {'Name': 'Genres - Custom Similarity', 'List': rec_enriched}
 
 
 def get_recommendation_list_3(movie_id):
-    rec = cbr.get_similar_movies(movie_id, [{'weight': 1, 'measure': title_levenshtein_similarity}])
+    rec = cbr.get_similar_movies(movie_id, [{'weight': 1,
+                                             'measure': set_based_similarity(jaccard_index, SetExtractor.KEYWORD)}])
     rec_enriched = [enrich_with_data(rec[0]) for rec in rec]
-    return {'Name': 'Levenshtein Title', 'List': rec_enriched}
+    return {'Name': 'Keywords - Jaccard Index', 'List': rec_enriched}
 
 
 def get_recommendation_list_4(movie_id):
+    rec = cbr.get_similar_movies(movie_id, [{'weight': 1, 'measure': title_levenshtein_similarity}])
+    rec_enriched = [enrich_with_data(rec[0]) for rec in rec]
+    return {'Name': 'Title - Levenshtein Distance', 'List': rec_enriched}
+
+
+def get_recommendation_list_5(movie_id):
     rec = cbr.get_similar_movies(movie_id, [{'weight': 1, 'measure': description_cosine_similarity}])
     rec_enriched = [enrich_with_data(rec[0]) for rec in rec]
-    return {'Name': 'TF-IDF Description', 'List': rec_enriched}
+    return {'Name': 'Description - TF-IDF, Cosine Similarity', 'List': rec_enriched}
 
 
 # def get_recommendation_list_5(movie_id):
 #     rec = cfr.get_movie_recommendations(movie_id)
-#     if len(rec) < 1:
-#         # it is not possible to perform item-based collaborative filtering for all movies, as cf is done with a
-#         # smaller dataset
-#         print("director set intersection cardinality was used for movie with id {}".format(movie_id))
+#     if len(rec) < 5:
+#         # it is not always possible to use collaborative filtering, because a smaller ratings.csv is not
+#         # from a smaller dataset
+#         print("director set and writer set intersection cardinality was used for movie with id {}".format(movie_id))
 #         rec = cbr.get_similar_movies(movie_id, [
-#             {'weight': 1, 'measure': set_based_similarity(intersection_cardinality, SetExtractor.DIRECTOR)}])
+#             {'weight': 0.5, 'measure': set_based_similarity(intersection_cardinality, SetExtractor.DIRECTOR)},
+#             {'weight': 0.5, 'measure': set_based_similarity(intersection_cardinality, SetExtractor.WRITER)}])
+#         rec_enriched = [enrich_with_data(rec[0]) for rec in rec]
+#         return {'Name': 'Intersection Cardinality Directors and Writers', 'List': rec_enriched}
 #     rec_enriched = [enrich_with_data(rec[0]) for rec in rec]
 #     return {'Name': 'Collaborative filteríng', 'List': rec_enriched}
 
 
-def get_recommendation_list_5(movie_id):
-    rec = gpt.get_similar_movies(movie_id)
-    if len(rec) < 5:
-        # it is not always possible to retrieve 5 movie recommendations from the API
-        print("director set intersection cardinality was used for movie with id {}".format(movie_id))
-        rec = cbr.get_similar_movies(movie_id, [
-            {'weight': 1, 'measure': set_based_similarity(intersection_cardinality, SetExtractor.DIRECTOR)}])
-        rec_enriched = [enrich_with_data(rec[0]) for rec in rec]
-        return {'Name': 'Intersection Cardinality Director', 'List': rec_enriched}
-    rec_enriched = [enrich_with_data(rec[0]) for rec in rec]
-    return {'Name': 'GPT Recommendations', 'List': rec_enriched}
-
-
 def enrich_with_data(movie_id):
-    # TODO: add more data for display
     movie_metadata = loader.get_movies_metadata()
     title = movie_metadata.loc[movie_metadata['movieId'] == movie_id, 'title'].values
     genres = movie_metadata.loc[movie_metadata['movieId'] == movie_id, 'movielens_genres'].values
@@ -89,16 +86,6 @@ def enrich_with_data(movie_id):
         'Title': title[0] if len(title) > 0 else '',
         'Genres': list(genres[0]) if len(genres) > 0 else '',
         'Rating': rating[0] if len(rating) > 0 else 0,
-    }
-
-
-def enrich_with_title(movie_title):
-    # TODO: add more data for display
-    movie_metadata = loader.get_movies_metadata()
-    movie_id = movie_metadata.loc[movie_metadata['title'] == movie_title, 'movieId'].values
-    return {
-        'MovieID': int(movie_id[0]) if len(movie_id) > 0 else 0,
-        'Title': movie_title,
     }
 
 
